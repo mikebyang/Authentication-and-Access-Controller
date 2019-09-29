f71e4d10dd5b2d1581f0e8f6df80eef57c88a847
Michael Yang <mcy@nbp-37-100.nbp.ruw.rutgers.edu>
2019-09-29 19:46:13 -0400

    Initial Commit
    the code that i have written so far

---
+++ authacccntrl.py
@@ -0,0 +1,143 @@
+import json
+import sys
+
+users = {}
+user_grps = {}
+obj_grps = {}
+acc_per = {}
+
+def req_length(minlength, maxlength):
+    """
+    Parameters-
+    minlength: minimum number of CLI arguments that are required
+    maxlengthL maximum number of CLI arguments that can be given
+
+    Return-
+    object determining if arguments passed satisfy conditions
+    """
+    return
+
+def save(diction, key):
+    """
+    Parameters-
+    diction: Dictionary to be saved
+    key: JSON file key to be used
+
+    Return-
+    0 if success and 1 otherwise
+    """ 
+    data = {}
+    data.setdefault(key,[])
+    for dkey,value in diction.items():
+        data[key].append({dkey:value})
+    with open(key, 'w') as fw:
+        json.dump(data,fw)
+    print("Success: File has been saved.")
+    return
+
+def load(FileName):
+    """
+    Parameter-
+    FileName: Name of the json file to be read (String)
+
+    Return-
+    Dictionary containing the lowest value pair stored in the json file
+    """
+    with open(FileName) as fw:
+        diction={}
+        data = json.load(fw)
+        for JsonKey,JsonValue in data.items():
+            for pair in JsonValue:
+                for key,value in pair.items():
+                    diction.update({key:value})
+
+def setUsers(diction):
+    users=diction
+
+def setUserGrps(diction):
+    user_grps=diction
+
+def setObjGrps(diction):
+    obj_grps=diction
+
+def setAccPer(diction):
+    acc_per=diction
+
+def AddUser(User, Passw):
+    """
+    Parameters-
+    User: New user to be added (String)
+    Passw: Password for the new user (String)
+
+    Return-
+    Dictionary key-value pair using User as the key and Passw as the value
+    """
+    if users.get(User) != None:
+        print("Failure: User already in the system!")
+        sys.exit(1)
+    else:
+        users.update({User: Passw})
+        print("Success: User has been added into the system!")
+    return
+
+def Authenticate(User, Passw):
+    if users.get(User) == None:
+        print("Failure: no such user!")
+    elif users.get(User) != Passw:
+        print("Failure: bad password!")
+    else:
+        print("Success!")
+    return
+
+def printgrp(Group):
+    for name in Group:
+        print(name)
+    return
+
+def AddUserToGroup(User, Group):
+    if users.get(User) == None:
+        print("Failure: user does not exist!")
+    elif user_grps.get(Group) == None:
+        user_grps.update({Group:[]})
+        user_grps[Group].append(User)
+        print("Success: user has been added to the group.")
+        print(User)
+    else:
+        try:
+            user_grps[Group].index(User)
+            print("User already in group.")
+        except:
+            user_grps[Group].append(User)
+            print("Success: user has been added to the group.")
+            printgrp(user_grps.get(Group))
+    return
+    
+def AddObjectToGroup(Obj, Group):
+    if users.get(Obj) == None:
+        print("Failure: user does not exist!")
+    elif user_grps.get(Group) == None:
+        user_grps.update({Group:[]})
+        user_grps[Group].append(Obj)
+        print("Success: object has been added to the group.")
+        print(Obj)
+    else:
+        try:
+            obj_grps[Group].index(Obj)
+            print("Object already in group.")
+        except:
+            obj_grps[Group].append(Obj)
+            print("Success: object has been added to the group.")
+            printgrp(user_grps.get(Group))
+    return
+
+def AddAccess(Operation, UserGrp, ObjGrp):
+    if ObjGrp == None:
+        return
+    else:
+        return
+
+def CanAccess(Operation, User, Obj):
+    if Obj == None:
+        return
+    else:
+        return
---
+++ core.py
@@ -0,0 +1,52 @@
+import argparse as ap
+import sys
+import authacccntrl as aac
+
+#load and save load only relevant files
+#command line parsing
+parser = ap.ArgumentParser(description="Simple authentication and access library")
+#possible flags
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-addu","--adduser",help="Define a new user for the system along with the user’s password, both strings.",nargs=2)
+group.add_argument("-auth","--authenticate",help="Validate a user’s password by passing the username and password, both strings.",nargs=2)
+group.add_argument("-addug","--addusertogroup",help="Add a user to a user group. If the group name does not exist, it is created. If a user does not exist, the function should return an error.",nargs=2)
+group.add_argument("-addog","--addobjecttogroup",help="Add an object to an object group. If the group name does not exist, it is created. The object can be any string.",nargs=2)
+group.add_argument("-adda","--addaccess",help="Define an access right: a string that defines an access permission of a user group to an object group. The access permission can be an arbitrary string that makes sense to the service.",nargs='+',action=aac.req_length(2,3))
+group.add_argument("-cana","--canaccess",help="Test whether a user can perform a specified operation on an object. Optionally, an object may be NULL, in which case CanAccess checks allows access if a user is part of a group for an operation on which no object group was defined.",nargs='+',action=aac.req_length(2,3))
+args = parser.parse_args()
+#load the correct files
+if args.adduser or args.authenticate:
+    try:
+        aac.setUsers(aac.load("users"))
+    except FileNotFoundError as FNFE:
+        print("No user file was found.")
+    except Exception as e:
+        print("Unknown error has occured:")
+        print(e)
+elif args.addusertogroup or args.addobjecttogroup:
+    aac.setUserGrps(aac.load("user_grps"))
+    aac.setObjGrps(aac.load("obj_grps"))
+elif args.addaccess or args.canaccess:
+    aac.setAccPer(aac.load("acc_per"))
+#execution
+if args.adduser:
+    aac.AddUser(args.adduser[0], args.adduser[1])
+elif args.authenticate:
+    aac.Authenticate(args.authenticate[0],args.authenticate[1])
+elif args.addusertogroup:
+    aac.AddUserToGroup(args.addusertogroup[0],args.addusertogroup[1])
+elif args.addobjecttogroup:
+    aac.AddObjectToGroup(args.addobjecttogroup[0],args.addobjecttogroup[1])
+elif args.addaccess:
+    aac.AddAccess()
+elif args.canaccess:
+    aac.CanAccess()
+#save to correct files and exit
+if args.adduser or args.authenticate:
+    aac.save(aac.users, "users")
+elif args.addusertogroup or args.addobjecttogroup:
+    aac.save(aac.user_grps, "user_grps")
+    aac.save(aac.obj_grps,"obj_grps")
+elif args.addaccess or args.canaccess:
+    aac.save()
+sys.exit(0)
