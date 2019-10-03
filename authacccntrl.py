e5b17626803a4c2488281f2046bd376857ef9cdb
Michael Yang <mcy@nbp-137-66.nbp.ruw.rutgers.edu>
2019-10-03 14:16:06 -0400

    Update
    added the code for several of the functions and added method to make sure number of inputs for add access and can access is >=2 and <=3

--- authacccntrl.py
+++ authacccntrl.py
@@ -6,25 +6,13 @@ user_grps = {}
 obj_grps = {}
 acc_per = {}
 
-def req_length(minlength, maxlength):
-    """
-    Parameters-
-    minlength: minimum number of CLI arguments that are required
-    maxlengthL maximum number of CLI arguments that can be given
-
-    Return-
-    object determining if arguments passed satisfy conditions
-    """
-    return
-
 def save(diction, key):
     """
+    Save the dictionary to file with the name stored in key
+
     Parameters-
     diction: Dictionary to be saved
     key: JSON file key to be used
-
-    Return-
-    0 if success and 1 otherwise
     """ 
     data = {}
     data.setdefault(key,[])
@@ -32,16 +20,18 @@ def save(diction, key):
         data[key].append({dkey:value})
     with open(key, 'w') as fw:
         json.dump(data,fw)
-    print("Success: File has been saved.")
+    print("Success: %s file has been saved."%(key))
     return
 
 def load(FileName):
     """
+    Load the dictionary from file
+
     Parameter-
     FileName: Name of the json file to be read (String)
 
     Return-
-    Dictionary containing the lowest value pair stored in the json file
+    Dictionary containing the dictionary stored in the json file
     """
     diction={}
     with open(FileName) as fw:
@@ -54,48 +44,73 @@ def load(FileName):
 
 def AddUser(User, Passw):
     """
-    Parameters-
-    User: New user to be added (String)
-    Passw: Password for the new user (String)
+    Adds user-password pair into the system
 
-    Return-
-    Dictionary key-value pair using User as the key and Passw as the value
+    Parameters-
+    User: New user to be added (string)
+    Passw: Password for the new user (string)
     """
     if users.get(User) != None:
         print("Failure: User already in the system!")
         sys.exit(1)
     else:
-        users.update({User: Passw})
+        users.setdefault(User, Passw)
         print("Success: User has been added into the system!")
     return
 
 def Authenticate(User, Passw):
+    """
+    Checks if user-password pair is in the system
+
+    Parameters-
+    User- User in the system (string)
+    Passw- Password for the user (string)
+    """
     if users.get(User) == None:
         print("Failure: no such user!")
+        sys.exit(1)
     elif users.get(User) != Passw:
         print("Failure: bad password!")
+        sys.exit(1)
     else:
         print("Success!")
         print("Access granted!")
     return
 
 def printgrp(Group):
+    """
+    Prints the users in the group
+
+    Parameter-
+    Group: List of users
+    """
     for name in Group:
         print(name)
     return
 
 def AddUserToGroup(User, Group):
+    """
+    Adds existing user to existing group or new group
+    
+    Parameters-
+    User: Existing user in the system (string)
+    Group: Existing group or name of group to be created (string)
+    """
     if users.get(User) == None:
+        #user exists in the system
         print("Failure: user does not exist!")
+        sys.exit(1)
     elif user_grps.get(Group) == None:
-        user_grps.update({Group:[]})
+        user_grps.setdefault(Group,[])
         user_grps[Group].append(User)
         print("Success: user has been added to the group.")
         print(User)
     else:
         try:
+            #user is in the group
             user_grps[Group].index(User)
             print("User already in group.")
+            sys.exit(1)
         except:
             user_grps[Group].append(User)
             print("Success: user has been added to the group.")
@@ -103,31 +118,142 @@ def AddUserToGroup(User, Group):
     return
     
 def AddObjectToGroup(Obj, Group):
-    if users.get(Obj) == None:
-        print("Failure: user does not exist!")
-    elif user_grps.get(Group) == None:
-        user_grps.update({Group:[]})
-        user_grps[Group].append(Obj)
+    """
+    Adds object to existing group or new group
+
+    Parameters-
+    Obj: Object name (string)
+    Group: Existing group or name of group to be created (string)
+    """
+    if obj_grps.get(Group) == None:
+        #object group does not exist
+        obj_grps.setdefault(Group,[])
+        obj_grps[Group].append(Obj)
         print("Success: object has been added to the group.")
         print(Obj)
     else:
-        try:
-            obj_grps[Group].index(Obj)
+        if Obj in obj_grps[Group]:
+            #object exists in the group
             print("Object already in group.")
-        except:
+            sys.exit(1)
+        else:
+            #object does not in the group
             obj_grps[Group].append(Obj)
             print("Success: object has been added to the group.")
-            printgrp(user_grps.get(Group))
+            printgrp(obj_grps.get(Group))
     return
 
-def AddAccess(Operation, UserGrp, ObjGrp):
-    if ObjGrp == None:
-        return
+def argscheck(arglist):
+    """
+    Checks if number of parameters passed is between 2 and 3 inclusive
+
+    Parameters-
+    arglist: list of parameters
+
+    Return-
+    Boolean value determining if the correct number of parameters was passed
+    """
+    if len(arglist)<2 or len(arglist)>3:
+        return False
     else:
-        return
+        return True
+
+def AddAccess(Operation, UserGrp, ObjGrp):
+    """
+    Gives user group access to an operation
+    
+    Parameters-
+    Operation: operation that acces is being granted for (string)
+    UserGrp: user group that is gaining access (string)
+    ObjGrp: object group in which the user group is being allowed to use the operation on (string)
+    ObjGrp(None): case in which user group has universal access to operation
+    """
+    if UserGrp not in user_grps:
+        #user group does not exist
+        print("Error: user group does not exist.")
+        sys.exit(1)
+    elif ObjGrp == None:
+        #no object group is provided
+        if acc_per.get(Operation) == None:
+            #operation does not exist in access dictionary
+            acc_per.setdefault(Operation, {UserGrp:None})
+        else:
+            acc_per[Operation].update({UserGrp:None})
+        print("%s operation access has been granted for user group, %s."%(Operation,UserGrp))
+    elif ObjGrp not in obj_grps:
+        #object group does not exist
+        print("Error: object group does not exist.")
+        sys.exit(1)
+    else:
+        if acc_per.get(Operation) == None:
+            #operation does not exist in access dictionary
+            acc_per.setdefault(Operation, {})
+            acc_per[Operation].setdefault(UserGrp,[])
+            acc_per[Operation][UserGrp].append(ObjGrp)
+        else:
+            if UserGrp not in acc_per[Operation]:
+                #if user has not been given permission to this operation before
+                acc_per[Operation].setdefault(UserGrp,[])
+                acc_per[Operation][UserGrp].append(ObjGrp)
+            elif ObjGrp in acc_per[Operation][UserGrp]:
+                print("%s already has permission for performing %s on object group, %s."%(UserGrp,Operation,ObjGrp))
+                sys.exit(1)
+            else:
+                acc_per[Operation][UserGrp].append(ObjGrp)
+        print("%s operation access for object group, %s, has been granted to user group, %s."%(Operation,ObjGrp,UserGrp))
+    return
+
+def checkUser(operation, user):
+    """
+    Checks to see if user has access to the operation.
+
+    Parameters-
+    operation: operation in question
+    user: user that may or may not have access to the operation
+
+    Return-
+    boolean which describes if user has access to operation and group with user in it
+    """
+    for usegroup in acc_per[operation]:
+        if user in user_grps[usegroup]:
+            return True,usegroup
+    return False,None
 
 def CanAccess(Operation, User, Obj):
+    """
+    Checks operation access permissions for a user
+
+    Parameters-
+    Operation: operation name (string)
+    User: name of user whose access permissions is being checked (string)
+    Obj: obj name (string)
+    Obj(None): user only has access if addaccess was used without an ObjGrp field
+    """
+    #find if operation permission has been granted to any user group
+    if Operation not in acc_per:
+        print("Error: permission for operation has not been granted to any user groups.")
+        sys.exit(1)
+    check, group = checkUser(Operation,User)
     if Obj == None:
-        return
+        #object field is not provided
+        if check:
+            #user is in a group with access to operation
+            if acc_per[Operation][group] == None:
+                print("%s is in a group with universal access to operation, %s."%(User,Operation))
+            else:
+                print("Error: user is in a group that does not have universal access to operation, %s."%(Operation))
+                sys.exit(1)
+        else:
+            print("Error: user is not in a group with access to operation, %s."%(Operation))
+            sys.exit(1)
     else:
-        return
+        if check:
+            #user is in a group with access to operation
+            if acc_per[Operation][group] == None:
+                print("%s has permission to perform operation, %s, on object, %s."%(User,Operation,Obj))
+            elif Obj in acc_per[Operation][group]:
+                print("%s has permission to perform operation, %s, on object, %s."%(User,Operation,Obj))
+        else:
+            print("Error: user is not in a group with access to operation, %s."%(Operation))
+            sys.exit(1)
+    return
--- core.py
+++ core.py
@@ -2,33 +2,45 @@ import argparse as ap
 import sys
 import authacccntrl as aac
 
-#load and save load only relevant files
 #command line parsing
-parser = ap.ArgumentParser(description="Simple authentication and access library")
+parser = ap.ArgumentParser(prog="Authentication and Access",description="Simple authentication and access library")
 #possible flags
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-addu","--adduser",help="Define a new user for the system along with the user’s password, both strings.",nargs=2)
-group.add_argument("-auth","--authenticate",help="Validate a user’s password by passing the username and password, both strings.",nargs=2)
-group.add_argument("-addug","--addusertogroup",help="Add a user to a user group. If the group name does not exist, it is created. If a user does not exist, the function should return an error.",nargs=2)
-group.add_argument("-addog","--addobjecttogroup",help="Add an object to an object group. If the group name does not exist, it is created. The object can be any string.",nargs=2)
-group.add_argument("-adda","--addaccess",help="Define an access right: a string that defines an access permission of a user group to an object group. The access permission can be an arbitrary string that makes sense to the service.",nargs='+',action=aac.req_length(2,3))
-group.add_argument("-cana","--canaccess",help="Test whether a user can perform a specified operation on an object. Optionally, an object may be NULL, in which case CanAccess checks allows access if a user is part of a group for an operation on which no object group was defined.",nargs='+',action=aac.req_length(2,3))
+group.add_argument("-addu","--adduser",help="Define a new user for the system along with the user’s password, both strings. (User, Password)",nargs=2)
+group.add_argument("-auth","--authenticate",help="Validate a user’s password by passing the username and password, both strings. (User, Password)",nargs=2)
+group.add_argument("-addug","--addusertogroup",help="Add a user to a user group. If the group name does not exist, it is created. If a user does not exist, the function should return an error. (User, Group)",nargs=2)
+group.add_argument("-addog","--addobjecttogroup",help="Add an object to an object group. If the group name does not exist, it is created. The object can be any string. (Object, Group)",nargs=2)
+group.add_argument("-adda","--addaccess",help="Define an access right: a string that defines an access permission of a user group to an object group. The access permission can be an arbitrary string that makes sense to the service. (Operation, User Group, Object Group)",nargs='*')
+group.add_argument("-cana","--canaccess",help="Test whether a user can perform a specified operation on an object. Optionally, an object may be NULL, in which case CanAccess checks allows access if a user is part of a group for an operation on which no object group was defined. (Operation, User, Object)",nargs='*')
 args = parser.parse_args()
 #load the correct files
 try:
     if args.adduser or args.authenticate:
         aac.users = aac.load("users")
-    elif args.addusertogroup or args.addobjecttogroup:
+    elif args.addusertogroup:
+        aac.users = aac.load("users")
         aac.user_grps = aac.load("user_grps")
+    elif args.addobjecttogroup:
         aac.obj_grps = aac.load("obj_grps")
     elif args.addaccess or args.canaccess:
+        if args.addaccess != None:
+            if not aac.argscheck(args.addaccess):
+                parser.print_help()
+                sys.exit(1)
+        elif args.canaccess != None:
+            if not aac.argscheck(args.canaccess):
+                parser.print_help()
+                sys.exit(1)
+        aac.user_grps = aac.load("user_grps")
+        aac.obj_grps = aac.load("obj_grps")
         aac.acc_per = aac.load("acc_per")
 except FileNotFoundError as FNFE:
     print("No user file was found.")
+    print(FNFE)
 except Exception as e:
     print("Unknown error has occured:")
     print(e)
-#execution
+#function execution
 if args.adduser:
     aac.AddUser(args.adduser[0], args.adduser[1])
 elif args.authenticate:
@@ -38,16 +50,26 @@ elif args.addusertogroup:
 elif args.addobjecttogroup:
     aac.AddObjectToGroup(args.addobjecttogroup[0],args.addobjecttogroup[1])
 elif args.addaccess:
-    aac.AddAccess()
+    if len(args.addaccess) == 3:
+        aac.AddAccess(args.addaccess[0],args.addaccess[1],args.addaccess[2])
+    else:
+        aac.AddAccess(args.addaccess[0],args.addaccess[1],None)
 elif args.canaccess:
-    aac.CanAccess()
+    if len(args.canaccess) == 3:
+        aac.CanAccess(args.canaccess[0],args.canaccess[1],args.canaccess[2])
+    else:
+        aac.CanAccess(args.canaccess[0],args.canaccess[1],None)
 #save to correct files and exit
-if args.adduser or args.authenticate:
-    aac.save(aac.users, "users")
-elif args.addusertogroup or args.addobjecttogroup:
-    aac.save(aac.user_grps, "user_grps")
-    aac.save(aac.obj_grps,"obj_grps")
-elif args.addaccess or args.canaccess:
-    aac.save(aac.acc_per,"acc_per")
+try:
+    if args.adduser or args.authenticate:
+        aac.save(aac.users, "users")
+    elif args.addusertogroup:
+        aac.save(aac.user_grps, "user_grps")
+    elif args.addobjecttogroup:
+        aac.save(aac.obj_grps,"obj_grps")
+    elif args.addaccess or args.canaccess:
+        aac.save(aac.acc_per,"acc_per")
+except Exception as e:
+    print(e)
 print("Shutting down.....")
 sys.exit(0)
